49dec228b0127c69ff4fef8b1c6a8d55fba01668
Hengsheng-Liu <lhs7714@bu.edu>
2024-02-16 13:15:58 -0500

    finish routing

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -9,26 +9,29 @@ import {
   Keyboard,
   TextInput,
 } from "react-native";
-import  {firestore}  from "../../firebaseConfig"
-import {auth} from "../../firebaseConfig"
+import { firestore } from "../../firebaseConfig";
 import { router } from "expo-router";
-import {createUserWithEmailAndPassword, UserCredential } from "firebase/auth";
+import { UserCredential } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
+import { useAuth} from "../../Context/AuthContext";
+
+
 
 export default function SignUpScreen() {
   const [email, setEmail] = useState<string | undefined>();
   const [password, setPassword] = useState<string | undefined>();
   const [confirmPassword, setConfirmPassword] = useState<string | undefined>();
   const [passwordMatchError, setPasswordMatchError] = useState<boolean>(false);
-  const handlePasswordChange = (text:string) => {
+  const {CreateUser}= useAuth(); 
+  const handlePasswordChange = (text: string) => {
     setPassword(text);
     if (text !== confirmPassword) {
       setPasswordMatchError(true);
     } else {
       setPasswordMatchError(false);
     }
-  }; 
-  const handleConfirmPasswordChange = (text:string) => {
+  };
+  const handleConfirmPasswordChange = (text: string) => {
     setConfirmPassword(text);
     if (password !== text) {
       setPasswordMatchError(true);
@@ -37,44 +40,43 @@ export default function SignUpScreen() {
     }
   };
 
-  const AddUserToDB = async(response: UserCredential) =>{
+  const AddUserToDB = async (response: UserCredential) => {
     const user = response.user;
     const db = firestore;
     const User = await addDoc(collection(db, "Users"), {
       uid: user.uid,
       email: user.email,
-      name:"",
+      name: "",
       friends: [],
       Gym: "",
       CheckInHistory: [],
-      icon:"",
+      icon: "",
       Achievement: [],
-      GymExperience :"0"
-    
+      GymExperience: "0",
     });
     console.log("Document written with ID: ", User.id);
-  }
+  };
   const handleSignUp = async () => {
-    if(email && password && confirmPassword && password === confirmPassword) {
-        try {
-          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-          const user = userCredential.user;
-          console.log(user);
-          if (user) {
-            Alert.alert("Success", "User has been created");
-            await AddUserToDB(userCredential);
-            router.navigate("LogIn");
-          }
-        }catch(error:any) {
-          Alert.alert("Error", error.message);
+    if (email && password && confirmPassword && password === confirmPassword) {
+      try {
+        const userCredential = await CreateUser(email, password);
+        const user = userCredential.user;
+        console.log(user);
+        if (user) {
+          Alert.alert("Success", "User has been created");
+          await AddUserToDB(userCredential);
+          router.navigate("LogIn");
         }
-  }else{
-    if (password !== confirmPassword) {
-      Alert.alert("Error", "Passwords do not match")
+      } catch (error: any) {
+        Alert.alert("Error", error.message);
+      }
+    } else {
+      if (password !== confirmPassword) {
+        Alert.alert("Error", "Passwords do not match");
+      }
+      Alert.alert("Error", "Please fill in all fields");
     }
-      Alert.alert("Error", "Please fill in all fields")
-  }
-}
+  };
   return (
     <Pressable style={styles.contentView} onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.contentView}>
@@ -105,11 +107,9 @@ export default function SignUpScreen() {
               onChangeText={handleConfirmPasswordChange}
               secureTextEntry
             />
-            {passwordMatchError && (
-              <Text>Passwords do not match</Text>
-            )}
+            {passwordMatchError && <Text>Passwords do not match</Text>}
             <Button title="Sign Up" onPress={handleSignUp} />
-            <Button title="Go Back" onPress={()=>router.navigate("LogIn")} />
+            <Button title="Go Back" onPress={() => router.navigate("LogIn")} />
           </View>
         </View>
       </SafeAreaView>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,18 +4,20 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
+import { AuthProvider } from '@/Context/AuthContext';
 import { useColorScheme } from '@/components/useColorScheme';
-
+import { Slot } from 'expo-router';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
+/*
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: '(tabs)/LogIn',
 };
+*/
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -48,10 +50,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+      <AuthProvider>
+        <Slot /> 
+      </AuthProvider>
   );
 }
