c35893ae5ed4626c2f4c8311db73b0de25c6075a
Hengsheng-Liu <lhs7714@bu.edu>
2024-02-21 19:56:13 -0500

    try to build the app

---
+++ app/(tabs)/Map.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Map extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Map</Text>
+      </View>
+    )
+  }
+}
+
+export default Map
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Redirect, Tabs } from 'expo-router'; // Removed Stack import, as it's not used
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Redirect, Tabs } from "expo-router"; // Removed Stack import, as it's not used
 
-import { useAuth } from '../../Context/AuthContext';
+import { useAuth } from "../../Context/AuthContext";
 
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -13,18 +13,15 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const { User } = useAuth(); // Changed User to user
-  if (!User) {
+  if (User) {
     return <Redirect href="/LogIn" />;
   } else {
     return (
       <Tabs>
-        <Tabs.Screen
-          name="index"
-        />  
-      <Tabs.Screen
-        name="Home"
-      />  
-    </Tabs>
+        <Tabs.Screen name="index" />
+        <Tabs.Screen name="Home" />
+        <Tabs.Screen name="Map" />
+      </Tabs>
     );
   }
 }
