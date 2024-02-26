74402e0fcce85f627aa1510adfc77b0d79833ae9
Hengsheng-Liu <lhs7714@bu.edu>
2024-02-26 16:57:35 -0500

    add a index file

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet, Button } from "react-native";
+import { useEffect } from "react";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { router } from "expo-router";
+import { useAuth } from "../../Context/AuthContext";
+export default function TabOneScreen() {
+  const { User, SignOut } = useAuth();
+  const handleSignOut = async () => {
+    try {
+      await SignOut();
+      router.navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
