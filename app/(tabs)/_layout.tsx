bff0390c8c6db4be51eb932befe5567d66816d60
Axel Almaguer <axel.0619@hotmail.com>
2024-02-26 16:37:08 -0500

    commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Redirect, Tabs } from "expo-router"; // Removed Stack import, as it's not used
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Redirect, Tabs } from 'expo-router'; // Removed Stack import, as it's not used
 
-import { useAuth } from "../../Context/AuthContext";
-
-import UserProfilePage from '../../components/UserProfilePage'
+import { useAuth } from '../../Context/AuthContext';
 
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -24,32 +22,11 @@ export default function TabLayout() {
           name="index"
         />  
       <Tabs.Screen
-      
-        name="(FriendPage)"
-        options = {{
-          title: "Home",
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen 
-        name = "Location"
-        options = {{
-          title: "Location",
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-      name = "Map"
-      options = {{
-        title: "Map",
-        headerShown: false,
-      }}
-      />
-            <Tabs.Screen
-        name="Profile"
-      /> 
-
+        name="Home"
+      />  
     </Tabs>
+
+    
     );
   }
 }
