1eeb1b205e31d375101b49d42ce6513b18d93167
Axel Almaguer <axel.0619@hotmail.com>
2024-03-02 19:55:08 -0500

    added currUser to AuthContext

--- Context/AuthContext.tsx
+++ Context/AuthContext.tsx
@@ -1,38 +1,53 @@
 import { createContext, useContext, useEffect, useState} from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, User, UserCredential } from "firebase/auth";
 import { auth } from "../firebaseConfig";
+import {IUser, getUser} from "../components/FirebaseDataService"
+
 type AuthContextValue = {
     CreateUser: (email: string, password: string) => Promise<UserCredential>;
     SignIn: (email: string, password: string) => Promise<UserCredential>;
     User: User | null;
+    currUser: IUser | null;
     SignOut: () => Promise<void>;
 }
 const AuthContext = createContext<AuthContextValue | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
-    const [userRef, setUserRef] = useState(null);
+    const [currUser, setCurrUser] = useState<IUser | null>(null);
+
     const CreateUser = async (email: string, password: string) => {
         return await createUserWithEmailAndPassword(auth, email, password);
-    }
+    };
+
     const SignIn = async (email: string, password: string) => {
         return await signInWithEmailAndPassword(auth, email, password);
-    }
+    };
+
     const SignOut = async () => {
         return await auth.signOut();
-    }
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((CurrUser) => {
-            // console.log("user", CurrUser);
-            setUser(CurrUser);
+    };
 
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
+            // console.log("user", CurrUser);
+            setUser(currentUser);
+
+            if (currentUser) {
+                const userData = await getUser(currentUser.uid);
+                setCurrUser(userData);
+            } else {
+                setCurrUser(null);
+            }
         });
         return unsubscribe;
     }, []);
+
     const authContextValue: AuthContextValue = {
         CreateUser,
         SignIn,
         User: user,
+        currUser: currUser,
         SignOut
     };
 
@@ -52,3 +67,4 @@ export const useAuth = () => {
     
     return context;
 }
+
