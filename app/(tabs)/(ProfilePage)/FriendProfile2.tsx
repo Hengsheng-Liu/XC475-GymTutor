1d8ff62e8e26b4da9c8f2e313b0d9ac98b9f5113
Axel Almaguer <axel.0619@hotmail.com>
2024-04-14 17:51:54 -0400

    fixed friendPage error and shows empty achievements when friend has none selected

--- app/(tabs)/(ProfilePage)/FriendProfile2.tsx
+++ app/(tabs)/(ProfilePage)/FriendProfile2.tsx
@@ -183,7 +183,7 @@ const FriendProfilePage = () => {
 
                 <Description bio={userInfo.bio}/>
                 <Achievement display = {userInfo.display}/>
-                <History history = {userInfo.checkInHistory}/>
+                <History history = {userInfo.checkInHistory.map(x => x.day)}/>
               </Flex>
             )}
           </Box>
